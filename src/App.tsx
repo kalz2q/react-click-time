2593911f369c9a3626e84d28ced537d649df5a00
kalz2q <katsu95i@gmail.com>
2022-08-26 15:41:06 +0900

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
   return (
     <div className="App">
       <p>You clicked {count} times</p>
